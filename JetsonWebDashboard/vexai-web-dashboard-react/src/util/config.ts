498538d4d398ccdcf265ea6f37d516076c98015a
satsinush <aneedham0577@gmail.com>
2025-11-21 20:44:32 -0600

    changed socket ip to localhost

--- JetsonWebDashboard/vexai-web-dashboard-react/src/util/config.ts
+++ JetsonWebDashboard/vexai-web-dashboard-react/src/util/config.ts
@@ -5,7 +5,7 @@ import { images } from "./images";
  * General configuration for the application
  */
 export const config = {
-  socketIP: "10.42.0.1",
+  socketIP: "127.0.0.1",
   socketPort: "3030",
 
   /**
